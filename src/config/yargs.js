8185f3091a5e573aa45512dbe123841c67873a24
gwfernandez <gwfernandez@gmail.com>
2019-07-07 19:23:28 -0300

    optimizacion del yargs

--- src/config/yargs.js
+++ src/config/yargs.js
@@ -1,16 +1,12 @@
-const paramDescripcion = {
-    descripcion: { demand: true, alias: 'd', desc: 'descripcion de la tarea' }
-};
+const descripcion = { demand: true, alias: 'd', desc: 'descripcion de la tarea' };
 
-const paramCompletado = {
-    completado: { demand: false, alias: 'c', default: true, desc: 'indicador de tarea completada' }
-};
+const completado = { demand: false, alias: 'c', default: true, desc: 'indicador de tarea completada' };
 
 const argv = require('yargs')
-    .command('crear', 'Crea una tarea', { ...paramDescripcion })
-    .command('actualizar', 'Actualiza el estado de una tarea', { ...paramDescripcion, ...paramCompletado })
+    .command('crear', 'Crea una tarea', { descripcion })
+    .command('actualizar', 'Actualiza el estado de una tarea', { descripcion, completado })
     .command('listar', 'Muestra la lista de tareas pendientes')
-    .command('borrar', 'Borra una tarea de la lista', { ...paramDescripcion })
+    .command('borrar', 'Borra una tarea de la lista', { descripcion })
     .help()
     .argv;
 
